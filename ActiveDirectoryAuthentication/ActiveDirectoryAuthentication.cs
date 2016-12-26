94956dbea50aaaaf842436fc8d9dabcc7a04576f
Povyshev Nikolay <azskit@mail.ru>
2016-12-26 22:08:18 +0300

    fix

---
+++ ActiveDirectoryAuthentication/ActiveDirectoryAuthentication.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.DirectoryServices.AccountManagement;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CimPlugin.Plugin.Authentication;
+using CimPlugin.Plugin.Groups;
+
+namespace ActiveDirectoryAuthentication
+{
+    [Export(typeof(IAuthentication))]
+    [Export(typeof(IGroupCollector))]
+    public class ActiveDirectoryAuthentication : IAuthentication, IGroupCollector
+    {
+        #region IPlugin
+
+        public string Author
+        {
+            get
+            {
+                return "azskit";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "BuiltInUsersProvider";
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return "0.0.0.1";
+            }
+        }
+
+        public TextWriter WarningStream { get; set; }
+
+        public TextWriter ErrorStream { get; set; }
+
+        public TextWriter InfoStream { get; set; }
+        #endregion
+
+
+
+        #region IAuthentication
+
+        public IEnumerable<AuthenticationData> CollectUsers()
+        {
+            if (plan == null)
+                InitUsersPlan();
+
+            return GetWindowsUsers(plan);
+        }
+
+        #endregion
+
+        #region IGroupCollector
+        public IEnumerable<Group> CollectGroups()
+        {
+            if (plan == null)
+                InitUsersPlan();
+
+            return GetGroups(plan);
+        }
+        #endregion
+
+
+
+        PlanFileReader.UsersPlan plan = PlanFileReader.GetUsersPlan(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ActiveDirectoryUsers.xml"));
+
+        private void InitUsersPlan()
+        {
+            plan = PlanFileReader.GetUsersPlan(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ActiveDirectoryUsers.xml"));
+        }
+
+        /// <summary>
+        /// Список всех пользователей согласно плану пользователей
+        /// </summary>
+        /// <param name="Users"></param>
+        /// <returns></returns>
+        public IEnumerable<ActiveDirectoryUser> GetWindowsUsers(PlanFileReader.UsersPlan Users)
+        {
+            List<ActiveDirectoryUser> UserList = new List<ActiveDirectoryUser>();
+
+            foreach (PlanFileReader.DomainEntry dom in Users.DomainList)
+            {
+                int userscount = 0;
+
+                InfoStream?.WriteLine("Looking for users in {0}\\{1}", dom.DomainName, dom.MainGroup.GroupName);
+
+                PrincipalContext ctx = null;
+                try
+                {
+                    ctx = new PrincipalContext(ContextType.Domain, dom.DomainName);
+
+                    GroupPrincipal principal = GroupPrincipal.FindByIdentity(ctx, dom.MainGroup.GroupName);
+
+                    principal.GetMembers(recursive: true).
+                                            OfType<UserPrincipal>().
+                                            ToList().
+                                            ForEach((member) =>
+                                            {
+                                                UserList.Add(new ActiveDirectoryUser()
+                                                {
+                                                    DisplayName = member.DisplayName ?? member.SamAccountName,
+                                                    UserId = member.Sid.Value,
+                                                    UserName = member.SamAccountName,
+                                                    Domain = dom.DomainName
+                                                }); userscount++;
+                                            });
+
+                    InfoStream?.WriteLine("{0} users found", userscount);
+                }
+                catch (PrincipalServerDownException e)
+                {
+                    ErrorStream?.WriteLine(e.ToString());
+                }
+                catch (Exception e)
+                {
+                    //ErrorStream?.WriteLine(CustomUtilites.FormatException(e));
+                    ErrorStream?.WriteLine(e.ToString());
+                    throw;
+                }
+                finally
+                {
+                    ctx?.Dispose();
+                }
+            }
+
+            return UserList;
+        }
+
+        public IEnumerable<Group> GetGroups(PlanFileReader.UsersPlan Plan)
+        {
+            List<Group> GroupList = new List<Group>();
+
+            foreach (PlanFileReader.DomainEntry domainEntry in Plan.DomainList)
+            {
+                PrincipalContext ctx = null;
+                try
+                {
+                    ctx = new PrincipalContext(ContextType.Domain, domainEntry.DomainName);
+
+                    foreach (PlanFileReader.GroupEntry groupEntry in domainEntry.PermanentRoomGroups)
+                    {
+
+                        try
+                        {
+                            InfoStream?.WriteLine("Looking for group {0}\\{1}", domainEntry.DomainName, groupEntry.GroupName);
+
+                            GroupPrincipal principal = GroupPrincipal.FindByIdentity(ctx, groupEntry.GroupName);
+
+                            if (principal == null)
+                            {
+                                WarningStream?.WriteLine("Group {0}\\{1} not found", domainEntry.DomainName, groupEntry.GroupName);
+
+                                continue;
+                            }
+
+                            ActiveDirectoryGroup newGroup = new ActiveDirectoryGroup();
+                            newGroup.GroupId = principal.Sid.Value;
+                            newGroup.DisplayName = principal.DisplayName ?? principal.Name;
+
+                            InfoStream?.WriteLine("Looking for users in {0}\\{1}", domainEntry.DomainName, groupEntry.GroupName);
+
+                            principal.GetMembers(recursive: true).
+                                                OfType<UserPrincipal>().
+                                                    ToList().
+                                                    ForEach((member) =>
+                                                    {
+                                                        newGroup.UserIds.Add(member.Sid.Value);
+                                                    });
+
+                            InfoStream?.WriteLine("{0} users found", newGroup.UserIds.Count);
+
+                            GroupList.Add(newGroup);
+
+                        }
+                        catch (Exception e)
+                        {
+                            //Console.Error.WriteLine(CustomUtilites.FormatException(e));
+                            ErrorStream?.WriteLine(e.ToString());
+                            throw;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    ErrorStream?.WriteLine(e);
+                }
+                finally
+                {
+                    ctx?.Dispose();
+                }
+            }
+
+            return GroupList;
+        }
+
+
+
+        public class ActiveDirectoryGroup : CimPlugin.Plugin.Groups.Group
+        {        }
+
+
+
+
+        public class ActiveDirectoryUser : AuthenticationData
+        {
+            //internal override bool CheckPassword(string password)
+            //{
+            //    throw new NotImplementedException();
+            //}
+
+            public override bool CheckPassword(string password, out string error)
+            {
+                error = "";
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
---
+++ CimPlugin/Plugin/PluginManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CimPlugin.Plugin
+{
+    //    class PluginManager
+    //    {
+
+    //        /// <summary>
+    //        /// Поток для информационных сообщений
+    //        /// </summary>
+    //        public TextWriter InfoStream { get; set; }
+
+    //        /// <summary>
+    //        /// Поток для  сообщений об ошибках
+    //        /// </summary>
+    //        public TextWriter ErrorStream { get; set; }
+
+
+
+    //        private List<IPlugin> plugins;
+
+    //        private void LoadPlugins()
+    //        {
+    //            try
+    //            {
+    //                plugins = new List<IPlugin>();
+    //                string folder = System.AppDomain.CurrentDomain.BaseDirectory;
+    //                string[] files = System.IO.Directory.GetFiles(folder, "*.dll");
+
+    //                foreach (string file in files)
+    //                {
+    //                    IPlugin plugin = IsPlugin(file);
+    //                    if (plugin != null)
+    //                        plugins.Add(plugin);
+    //                }
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                ErrorStream.WriteLine("{0} : An error has occured during loading plugins : {1}", DateTime.Now.ToString(), e.Message);
+    //                //ErrorStream.WriteLine("{0} : An error has occured during loading plugins : {1}", DateTime.Now.ToString(), CustomUtilites.FormatException(e));
+    //            }
+    //        }
+
+    //        private IPlugin IsPlugin(byte[] file)
+    //        {
+    //            System.Reflection.Assembly assembly = System.Reflection.Assembly.Load(file);
+    //            foreach (Type type in assembly.GetTypes())
+    //            {
+    //                Type iface = type.GetInterface("Plugin.IPlugin");
+    //                if (iface != null)
+    //                {
+    //                    IPlugin plugin = (IPlugin)Activator.CreateInstance(type);
+    //                    return plugin;
+    //                }
+    //            }
+    //            return null;
+    //        }
+
+    //        private IPlugin IsPlugin(string file_url)
+    //        {
+    //            try
+    //            {
+    //                byte[] b = System.IO.File.ReadAllBytes(file_url);
+    //                return IsPlugin(b);
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                ErrorStream.WriteLine("{0} : An error has occured during loading file {1}: {2}", DateTime.Now.ToString(), file_url, e.Message);
+    //                //ErrorStream.WriteLine("{0} : An error has occured during loading file {1}: {2}", DateTime.Now.ToString(), file_url, CustomUtilites.FormatException(e));
+    //            }
+    //            return null;
+    //        }
+    //    }
+}
